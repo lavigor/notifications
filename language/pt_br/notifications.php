8c81788636b4f553c7bf1791721bb57b06d28665
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-01-23 12:35:31 -0200

    update

--- language/pt_br/notifications.php
+++ language/pt_br/notifications.php
@@ -22,16 +22,16 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'LAVIGOR_NOTIFICATIONS_NOTIFICATION_METHOD_BROWSER' => 'Navegador',
-	'BROWSER_NOTIFICATIONS'                             => 'Notificações do navegador',
+	'BROWSER_NOTIFICATIONS'                             => 'Notificações no navegador',
 	'BROWSER_NOTIFICATIONS_ACTION_ENABLED'              => 'Ligado',
 	'BROWSER_NOTIFICATIONS_ACTION_DISABLED'             => 'Desligado',
 	'BROWSER_NOTIFICATIONS_ACTION_DISALLOWED'           => 'Não permitido',
 	'BROWSER_NOTIFICATIONS_ACTION_UNSUPPORTED'          => 'Não suportado',
-	'BROWSER_NOTIFICATIONS_ENABLE'                      => 'Ativar notificações do navegador.',
-	'BROWSER_NOTIFICATIONS_DISABLE'                     => 'Desativar notificações do navegador.',
+	'BROWSER_NOTIFICATIONS_ENABLE'                      => 'Ativar notificações no navegador.',
+	'BROWSER_NOTIFICATIONS_DISABLE'                     => 'Desativar notificações no navegador.',
 	'BROWSER_NOTIFICATIONS_DISALLOWED'                  => 'As Notificações Web não são permitidas neste navegador. Permita notificações para este site nas configurações do navegador se desejar se inscrever.',
 	'BROWSER_NOTIFICATIONS_INTRO'                       => 'Deseja permitir que este portal o avise sobre as respostas?',
-	'BROWSER_NOTIFICATIONS_MAX_LIMIT_REACHED'           => 'O número máximo permitido de navegadores com notificações de navegador ativadas  (%s por usuário) foi alcançado. Por favor, desligue as notificações do navegador para este fórum em qualquer outro navegador onde você as ativou se desejar ativar as notificações de navegador neste navegador.',
-	'BROWSER_NOTIFICATIONS_UNSUPPORTED'                 => 'As notificações da Web não são suportadas por este navegador.',
-	'BROWSER_NOTIFICATIONS_UPDATE_FAILED'               => 'Ocorreu um erro (possivelmente erro de conexão ao entrar em contato com o servidor) durante a assinatura de notificações da Web. Eles foram desativados neste navegador. Por favor verifique sua conexão com a internet e tente novamente. Entre em contato com o administrador do fórum se o problema persistir.',
+	'BROWSER_NOTIFICATIONS_MAX_LIMIT_REACHED'           => 'O número máximo permitido de navegadores com notificações no navegador ativadas  (%s por usuário) foi alcançado. Por favor, desligue as notificações no navegador para este fórum em qualquer outro navegador onde você as ativou se desejar ativar as notificações de navegador neste navegador.',
+	'BROWSER_NOTIFICATIONS_UNSUPPORTED'                 => 'As notificações Web não são suportadas por este navegador.',
+	'BROWSER_NOTIFICATIONS_UPDATE_FAILED'               => 'Ocorreu um erro (possivelmente erro de conexão ao entrar em contato com o servidor) durante a assinatura de notificações Web. Eles foram desativados neste navegador. Por favor verifique sua conexão com a internet e tente novamente. Entre em contato com o administrador do fórum se o problema persistir.',
 ));
